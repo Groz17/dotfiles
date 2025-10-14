f9a3b55a8bbd32ff5bf56f628ab17c76b9563875
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-10-14 23:34:10 +0200

    add optimization

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((org-mode . ((eval . (when (locate-library "org")
                         (require 'ob-lob)
-                        (org-babel-lob-ingest (expand-file-name "_lib.org" default-directory))))
+                        (org-babel-lob-ingest (expand-file-name "_lib|.org" default-directory))))
               (org-babel-default-header-args . ((:mkdirp . "yes"))))))
