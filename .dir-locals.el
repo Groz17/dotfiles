af9f1a94148384de9b46625e4050f750c628d7c3
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-10-17 22:56:20 +0200

    only tangleable files should have mkdirp yes

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
-((org-mode . ((eval . (when (locate-library "org")
-                        (require 'ob-lob)
-                        (org-babel-lob-ingest (expand-file-name "_lib|.org" default-directory))))
-              (org-babel-default-header-args . ((:mkdirp . "yes"))))))
+((org-mode . ((eval . (progn
+                        (when (locate-library "org")
+                          (require 'ob-lob)
+                          (org-babel-lob-ingest (expand-file-name "_lib|.org" default-directory)))
+                        (when (and buffer-file-name
+                                   (string-match-p "[^|]\\.org\\'" (file-name-nondirectory buffer-file-name)))
+                          (setq-local org-babel-default-header-args
+                                      (cons '(:mkdirp . "yes") org-babel-default-header-args))))))))
